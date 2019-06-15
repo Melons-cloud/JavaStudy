1967da89bd8cbf4d4c9590ac8c9bcac3e3dc2d79
hjl19910901 <1urbtbri7>
2019-06-15 17:40:39 +0800

    调用成员方法创建对象

---
+++ src/com/hjl/day01/PhoneReturn.java
@@ -0,0 +1,19 @@
+package com.hjl.day01;
+
+public class PhoneReturn {
+    public static void main(String[] args) {
+        Phone two =getPhone();  //调用了getPhone()方法，因为里面有创建对象的方法，所以同时也就创建了对象two
+        System.out.println(two.brand);
+        System.out.println(two.price);
+        System.out.println(two.color);
+    }
+
+    public static Phone getPhone(){
+        Phone one = new Phone();
+        one.brand="苹果";
+        one.price=8388.0;
+        one.color="玫瑰金";
+        return one;
+
+    }
+}
