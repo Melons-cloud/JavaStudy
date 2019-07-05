00449bfd1375e2e3cf683bc3baf9cec30240cf0f
hjl19910901 <18710360575@163.com>
2019-07-06 02:06:20 +0800

    接口的实现类

--- src/com/hjl/Interface/MyInterfaceAbstractImpl.java
+++ src/com/hjl/Interface/MyInterfaceAbstractImpl.java
@@ -2,7 +2,13 @@ package com.hjl.Interface;
 
 public class MyInterfaceAbstractImpl implements MyinterfaceAbstract {
 
-
+    public static void main(String[] args) {
+       MyInterfaceAbstractImpl impl = new MyInterfaceAbstractImpl();
+       impl.mathodAbs1();
+       impl.mathodAbs2();
+       impl.mathodAbs3();
+       impl.mathodAbs4();
+    }
     @Override
     public void mathodAbs1() {
         System.out.println("这是第一个方法！");
