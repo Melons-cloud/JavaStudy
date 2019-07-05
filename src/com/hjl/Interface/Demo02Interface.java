2d38ad7d55b638ccdf3850ede8d7fe5613f9a48b
hjl19910901 <18710360575@163.com>
2019-07-06 02:37:05 +0800

    默认方法

--- src/com/hjl/Interface/Demo02Interface.java
+++ src/com/hjl/Interface/Demo02Interface.java
@@ -1,5 +1,10 @@
 package com.hjl.Interface;
-
+/*
+* 1.接口的默认方法，可以通过接口实现类对象，直接调用。
+* 2.接口的默认方法，也可以被接口实现类进行覆盖重写。
+*
+*
+* */
 public class Demo02Interface {
     public static void main(String[] args) {
 
--- src/com/hjl/Interface/MyInterfaceDefaultB.java
+++ src/com/hjl/Interface/MyInterfaceDefaultB.java
@@ -7,4 +7,9 @@ public class MyInterfaceDefaultB implements MyInterfaceDefault {
         System.out.println("实现了抽象方法，BBB");
 
     }
+
+    @Override
+    public void methodDefault() {
+        System.out.println("实现类B覆盖重写了接口的默认方法!");
+    }
 }
