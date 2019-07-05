02b56c6a3368685481b32280b40dd248e52fa932
hjl19910901 <18710360575@163.com>
2019-07-06 02:03:09 +0800

    抽象方法

---
+++ src/com/hjl/Interface/MyInterfaceAbstractImpl.java
@@ -0,0 +1,29 @@
+package com.hjl.Interface;
+
+public class MyInterfaceAbstractImpl implements MyinterfaceAbstract {
+
+
+    @Override
+    public void mathodAbs1() {
+        System.out.println("这是第一个方法！");
+    }
+
+    @Override
+    public void mathodAbs2() {
+
+        System.out.println("这是第二个方法！");
+    }
+
+    @Override
+    public void mathodAbs3() {
+
+        System.out.println("这是第三个方法！");
+
+    }
+
+    @Override
+    public void mathodAbs4() {
+        System.out.println("这是第四个方法！");
+
+    }
+}
