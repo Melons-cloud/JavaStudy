8b73c5073e425e0b80b892deaa4b9ef5dae159fc
hjl19910901 <18710360575@163.com>
2019-07-06 02:33:13 +0800

    默认方法

--- src/com/hjl/Interface/Demo02Interface.java
+++ src/com/hjl/Interface/Demo02Interface.java
@@ -6,7 +6,12 @@ public class Demo02Interface {
         //创建实现类对象
         MyInterfaceDefaultA a =new MyInterfaceDefaultA();
         a.methodAbs();//调用抽象方法，实际运行的是右侧实现类
-
+        //调用默认方法，如果实现类当中没有，会向上找接口。
         a.methodDefault();
+
+        System.out.println("======================");
+        MyInterfaceDefaultB b =new MyInterfaceDefaultB();
+        b.methodDefault();
+        b.methodAbs();
     }
 }
