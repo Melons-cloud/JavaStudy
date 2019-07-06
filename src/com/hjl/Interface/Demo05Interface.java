d17ceb23e89c62c8d8d8b75044a878ed66596b98
hjl19910901 <18710360575@163.com>
2019-07-06 12:06:16 +0800

    接口中的常量

---
+++ src/com/hjl/Interface/Demo05Interface.java
@@ -0,0 +1,11 @@
+package com.hjl.Interface;
+
+public class Demo05Interface {
+    public static void main(String[] args) {
+        //访问接口当中的常量
+
+
+        System.out.println(MyInterfaceConst.num);
+
+    }
+}
