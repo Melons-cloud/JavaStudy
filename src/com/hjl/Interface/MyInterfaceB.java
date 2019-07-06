19623e02e5dcc33f8d8325a51c09f4c8e4952873
hjl19910901 <18710360575@163.com>
2019-07-06 12:28:54 +0800

    接口中的常量

---
+++ src/com/hjl/Interface/MyInterfaceB.java
@@ -0,0 +1,14 @@
+package com.hjl.Interface;
+
+public interface MyInterfaceB {
+//   错误写法，接口不能有静态代码块。
+//    static {
+//
+//    }
+    //接口不能有构造方法
+//    public MyInterface(){
+//
+//    }
+public abstract void methodB();
+    public abstract void methodAbs();
+}
