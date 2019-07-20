76f134065f7abca797b80c9c3343eaa72a5d0bc6
hjl19910901 <18710360575@163.com>
2019-07-20 14:02:14 +0800

    Runnable接口类

--- src/com/hjl/Thread/Demo01Runnable.java
+++ src/com/hjl/Thread/Demo01Runnable.java
@@ -14,6 +14,15 @@ package com.hjl.Thread;
 * 4.创建Thread类对象,构造方法中传递Runnable端口的实现对象
 * 5.调用Thread类中的start方法,开始新的线程执行run方法
 *
+* 、、实现Runnable接口创建多线程的好处:
+* 1.避免单继承的局限性
+*       一个类只能继承一个类(一个人只能有一个亲爹)，类继承了Thread类就不能继承其他的类
+*       实现了Runnable接口，还可以继承其他的类，实现其他的接口
+* 2.增强了程序的扩展性，降低了程序的耦合性(解耦)
+*       实现Runnable接口的方式，把设置线程任务和开启线程进行了分离（解耦）
+*       实现类中，重写了run方法,用来设置线程
+*       创建Thread类对象,调用start方法，用来开启新线程
+*
 *
 * */
 public class Demo01Runnable {
@@ -23,8 +32,8 @@ public class Demo01Runnable {
         RunnableImpl run =new RunnableImpl();
 
         //创建Thread类对象，构造方法中传递Runnable接口的实现类对象
-        Thread t = new Thread(run);
-
+//        Thread t = new Thread(run);
+            Thread t =new Thread(new RunnableImpl2());  //打印Hello World
         //调用Thread类中的start方法,开启新的线程执行run方法
         t.start();
         for (int i=0;i<20;i++){
