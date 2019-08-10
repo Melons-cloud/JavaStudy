129ece49f5c91d6c5ab1f0505ec8bd3700cf80d8
hjl19910901 <18710360575@163.com>
2019-08-10 08:21:31 +0800

    文件信息

--- src/com/hjl/InputAndOutput/FileTest.java
+++ src/com/hjl/InputAndOutput/FileTest.java
@@ -9,8 +9,13 @@ public class FileTest {
         File file =new File("world.txt");    //创建文件对象
         if (file.exists()){    //判断该文件是否存在
 
-            file.delete();
-            System.out.println("文件已被删除");
+            String name =file.getName();  //获取文件名称
+            long length =file.length();  //获取文件长度
+            boolean hidden =file.isHidden();  //判断文件是否为隐藏文件
+            System.out.println("文件名称:"+name);
+            System.out.println("文件长度:"+length);
+            System.out.println("该文件是否为隐藏文件?"+hidden);
+
         }else {
 
             try {
