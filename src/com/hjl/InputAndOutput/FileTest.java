d939863337ca41e837714dbd633bf68c92e13d97
hjl19910901 <18710360575@163.com>
2019-08-10 08:34:28 +0800

    文件信息

--- src/com/hjl/InputAndOutput/FileTest.java
+++ src/com/hjl/InputAndOutput/FileTest.java
@@ -1,30 +1,46 @@
 package com.hjl.InputAndOutput;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class FileTest {
 
     public static void main(String[] args) {
         File file =new File("world.txt");    //创建文件对象
-        if (file.exists()){    //判断该文件是否存在
 
-            String name =file.getName();  //获取文件名称
-            long length =file.length();  //获取文件长度
-            boolean hidden =file.isHidden();  //判断文件是否为隐藏文件
-            System.out.println("文件名称:"+name);
-            System.out.println("文件长度:"+length);
-            System.out.println("该文件是否为隐藏文件?"+hidden);
-
-        }else {
 
             try {
-                file.createNewFile();   //创建该文件
-                System.out.println("文件已创建");
-            } catch (IOException e) {
+               //创建FileOutputStream对象
+                FileOutputStream fileOutputStream =new FileOutputStream(file);
+
+                //创建byte型数组
+                byte buy[] ="我有一只小毛驴，我一直都不骑。".getBytes();
+                //将数组中的信息写入到文件中
+                fileOutputStream.write(buy);
+                //关闭流
+                fileOutputStream.close();
+
+            } catch (Exception e) {
                 e.printStackTrace();
             }
+            try {
 
-        }
+                //创建FileInputStream类对象
+                FileInputStream fileInputStream  =new FileInputStream(file);
+                //创建byte数组
+                byte byt[] = new byte[1024];
+                //从文件中读取信息
+                int len = fileInputStream.read(byt);
+                //输出文件信息
+                System.out.println("文件的信息是:"+new String(byt,0,len));
+                //关闭流
+                fileInputStream.close();
+
+
+        }catch (Exception e){
+                e.printStackTrace();
+            }
     }
 }
