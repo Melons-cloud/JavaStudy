0b13778a1f662b647f046a1be04113f6ee4dda2c
hjl19910901 <18710360575@163.c0m>
2019-09-08 21:44:14 +0800

    更精确的使用浮点数

---
+++ src/com/hjl/JavaBasic/AccuratelyFloat.java
@@ -0,0 +1,21 @@
+package com.hjl.JavaBasic;
+
+import java.math.BigDecimal;
+
+public class AccuratelyFloat {
+    public static void main(String[] args) {
+        double money=2;   //现有金额
+        double price=1.1;  //商品价格
+        double result=money-price;
+        System.out.println("非精确计算");
+        System.out.println("剩余金额:"+result);  //输出运算结果
+        //精确浮点数的解决方法
+        BigDecimal money1 =new BigDecimal("2");   //现有金额
+        BigDecimal price1=new BigDecimal("1.1");   //点击商品
+        //当前对象执行减法操作
+        BigDecimal result1=money1.subtract(price1);
+        System.out.println("精确计算");
+        System.out.println("剩余金额:"+result1);  //输出精确计算的结果
+
+    }
+}
