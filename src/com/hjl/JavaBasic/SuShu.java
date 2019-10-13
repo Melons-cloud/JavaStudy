d6ec2698a67f9b4e559941fa2bd1b97eb6885dc5
hjl19910901 <18710360575@163.com>
2019-10-13 14:59:56 +0800

    素数

---
+++ src/com/hjl/JavaBasic/SuShu.java
@@ -0,0 +1,40 @@
+package com.hjl.JavaBasic;
+
+import java.util.Scanner;
+
+public class SuShu {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("请输入你要判断的数! ");
+
+        int x=scanner.nextInt();
+        int i=2,flage=0;
+        while (flage==0&&i<x){     //对除数进行遍历
+
+            if (x%i==0){     //判断是否被整除
+
+                flage=1;
+
+            }
+            else {
+
+                i++;
+            }
+
+        }
+
+        if (flage==0){      //对标记进行判断
+
+            System.out.println(x+"是素数! ");
+
+        }
+        else {
+
+
+            System.out.println(x+ "不是素数! ");
+        }
+
+    }
+}
